de70b0e9f22e449ba893c6323ec5491425aa05bb
Sheng Su <susheng0755@gmail.com>
2022-06-19 22:42:47 -0700

    s2 q2

--- Single-entity/q1.sql
+++ Single-entity/q1.sql
@@ -1,4 +1,4 @@
 -- Prepare a list of offices sorted by country, state, city.
 
-select territory from offices
-    order by country, state, city;
+select territory from offices   -- extract column "territory" from subtable "offices"
+    order by country, state, city;  -- sorted by "country", then by "state", and then by "city"
---
+++ Single-entity/q2.sql
@@ -0,0 +1,3 @@
+-- How many employees are there in the company?
+
+select count(*) from employees; -- show the count of all the records in the subtable "employees"
